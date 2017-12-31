7c81e88a68b9bc49f09187f5c1da349a63d475eb
Alex Kitchens <alexcameron98@gmail.com>
2017-12-31 13:34:26 -0600

    Add a test for a hard puzzle

--- src/solver.rs
+++ src/solver.rs
@@ -120,4 +120,34 @@ mod tests {
             solver.solve_puzzle();
             assert_eq!(correct_answer, solver.answer());
     }
+
+    #[test]
+    fn it_solves_a_hard_puzzle_correctly() {
+        let grid = vec![
+            vec![0,0,3,0,0,0,0,0,0],
+            vec![0,1,0,2,6,0,0,8,0],
+            vec![2,8,0,0,0,4,9,0,0],
+            vec![0,0,6,0,0,5,0,0,0],
+            vec![9,0,8,0,0,0,5,0,3],
+            vec![0,0,0,6,0,0,7,0,0],
+            vec![0,0,9,7,0,0,0,4,8],
+            vec![0,3,0,0,1,8,0,5,0],
+            vec![0,0,0,0,0,0,2,0,0]
+        ];
+        let correct_answer = vec![
+            vec![6,9,3,8,5,1,4,7,2],
+            vec![4,1,7,2,6,9,3,8,5],
+            vec![2,8,5,3,7,4,9,1,6],
+            vec![1,7,6,9,3,5,8,2,4],
+            vec![9,2,8,1,4,7,5,6,3],
+            vec![3,5,4,6,8,2,7,9,1],
+            vec![5,6,9,7,2,3,1,4,8],
+            vec![7,3,2,4,1,8,6,5,9],
+            vec![8,4,1,5,0,6,2,3,7]
+        ];
+            let puzzle = Puzzle { grid: grid };
+            let mut solver = Solver { puzzle: puzzle };
+            solver.solve_puzzle();
+            assert_eq!(correct_answer, solver.answer());
+    }
 }
